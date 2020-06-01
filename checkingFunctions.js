377f6cb4e7074a04a64f56a3a7a575098a2f0c9f
Yuliya Mysiura <julmisura@ukr.net>
2020-06-01 20:28:50 +0300

    Додано підтримку літер українського алфавіту (Є, І, Ї)

--- checkingFunctions.js
+++ checkingFunctions.js
@@ -5,10 +5,10 @@ function checkInput() {
 
 function checkName() {
     var elem = document.getElementById('nameInput');
-    elem.setAttribute('pattern', '[A-Za-zА-Яа-я]+');
+    elem.setAttribute('pattern', '[A-Za-zА-Яа-яІіЇїЄє]+');
 }
 
 function checkSurname() {
     var elem = document.getElementById('surnameInput');
-    elem.setAttribute('pattern', '[A-Za-zА-Яа-я]+');
+    elem.setAttribute('pattern', '[A-Za-zА-Яа-яІіЇїЄє]+');
 }
